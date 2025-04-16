9182a6281a6e82502643cf5a9a385854b72c2b9b
nMDaas <63910563+nMDaas@users.noreply.github.com>
2025-04-15 22:00:00 -0700

    Implemented ambient lighting

--- 3D/shaders/fragPhong.glsl
+++ 3D/shaders/fragPhong.glsl
@@ -8,7 +8,10 @@ out vec4 color;
 // Entry point of program
 void main()
 {
+    float ambient_strength = 0.1;
+    vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
+    vec3 ambient = ambient_strength * lightColor;
 	vec3 normals  = normalize(v_vertexNormals); 
-    vec3 lightColor_x_objectColor = vec3(1.0f, 1.0f, 1.0f) * vec3(normals.r, normals.g, normals.b);
+    vec3 lightColor_x_objectColor = ambient * vec3(normals.r, normals.g, normals.b);
 	color = vec4(lightColor_x_objectColor, 1.0f);
 }
