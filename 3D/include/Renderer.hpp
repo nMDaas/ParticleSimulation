ce4f24dc8fdcc2b2e2ef4f375761cccfaa9ab208
nMDaas <63910563+nMDaas@users.noreply.github.com>
2025-04-16 23:53:47 -0700

    Renderer.hpp

---
+++ 3D/include/Renderer.hpp
@@ -0,0 +1,53 @@
+// Third Party Libraries
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp> 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
+
+// C++ Standard Template Library (STL)
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <unordered_map>
+
+#include "Solver.hpp"
+#include "Particle.hpp"
+#include "Camera.hpp"
+#include "Vertex.hpp"
+#include "Triangle.hpp"
+
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+class Renderer{
+public:
+    Renderer();
+    Renderer(int i_screenWidth, int i_screenHeight);
+
+    void CreateGraphicsPipelines();
+    void PreDraw();
+    void DrawLights(Particle gLightParticle, GLuint lightVertexArrayObject, GLuint lightVertexBufferObject, int gTotalIndices, Camera gCamera);
+
+private:
+    int screenWidth;
+    int screenHeight; 
+
+    GLuint gGraphicsLighterPipelineShaderProgram = 0;
+
+    std::string LoadShaderAsString(const std::string& filename);
+    GLuint CreateShaderProgram(const std::string& vertexShaderSource, const std::string& fragmentShaderSource);
+    GLuint CompileShader(GLuint type, const std::string& source);
+
+    void PreDrawLight(Particle gLightParticle);
+    void DrawLight(GLuint lightVertexArrayObject, GLuint lightVertexBufferObject, int gTotalIndices);
+};
+
+
+
+#endif
